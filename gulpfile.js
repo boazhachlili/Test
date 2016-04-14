8fcf77873115f0044f143c99b137c558acaee597
Boaz Hachlili <boaz@genomecompiler.com>
2016-04-14 14:44:51 +0300

    testing TRAVIS CI, browserstack

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * (c) 2013-2015 Wishtack
+ *
+ * $Id: $
+ */
+
+var _ = require('underscore');
+var gulp = require('gulp');
+var requireDir = require('require-dir');
+
+// register tasks:
+_.forEach(requireDir('./gulp/tasks'), function (task, taskName) {
+    gulp.task(taskName, task);
+});
