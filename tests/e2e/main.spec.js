3d5ea964590e7d9bb1644b8a558eaa71ee48e4f6
Boaz Hachlili <boaz@genomecompiler.com>
2016-04-17 10:41:19 +0300

    test3

--- tests/e2e/main.spec.js
+++ tests/e2e/main.spec.js
@@ -2,16 +2,16 @@
 var PlasmidViewer = require('./page_objects/plasmid_viewer.js');
 
 // keep our globals namespaced:
-// GLOBAL.gc = {};
+GLOBAL.gc = {};
 
-// GLOBAL.gc.environment = browser.params.environment;
+GLOBAL.gc.environment = browser.params.environment;
 
-// GLOBAL.gc.utils = require('./utils.js');
+GLOBAL.gc.utils = require('./utils.js');
 
-// GLOBAL.gc.plasmidViewer = new PlasmidViewer(GLOBAL.gc.environment);
+GLOBAL.gc.plasmidViewer = new PlasmidViewer(GLOBAL.gc.environment);
 
 // describe("Test Header", require('./specs/header/header.js'));
 
-// describe("Test Sequence View", require('./specs/sequence_view/sequence_view_tests.js'));
+describe("Test Sequence View", require('./specs/sequence_view/sequence_view_tests.js'));
 
 // describe("Test Circular View"), require('./specs/circular_view/FOOBAR.js');
